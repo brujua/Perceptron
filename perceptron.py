5185c7b04f94828c1b07ef59b3c08f3cc61c47f7
brujua <brunocrisafulli@yahoo.com.ar>
2019-05-11 12:41:17 -0300

    Implemented training

--- perceptron.py
+++ perceptron.py
@@ -1,15 +1,15 @@
 import sys
+import numpy as np
 from random import randint
 from typing import List
 
-DIMENSION = 3
+DIMENSION = 2
 ERROR_ARGS = "Invalid Argument. \nUsage: python " + sys.argv[0] + " <data-set-file> \
 (tab separated, each data point in one line)"
-weights = []
+MAX_EPOCHS = 100000
+LEARNING_RATE = 0.1
 
-
-def train(training_points: List):
-    pass
+_weights = []
 
 
 def main(*args):
@@ -19,9 +19,42 @@ def main(*args):
     train(training_points)
 
 
+def train(training_points: List):
+    epoch = 0
+    finished = False
+    while not finished and epoch < MAX_EPOCHS:
+        finished = True
+        epoch += 1
+        for point in training_points:
+            result = evaluate(point[:DIMENSION])
+            desired_result = point[DIMENSION]
+            pass
+            if result != desired_result:
+                finished = False
+                for i in range(0, DIMENSION):
+                    if result < desired_result:
+                        _weights[i] += point[i] * LEARNING_RATE
+                        _weights[DIMENSION] += LEARNING_RATE  # Bias
+                    else:  # result is > than desired
+                        _weights[i] -= point[i] * LEARNING_RATE
+                        _weights[DIMENSION] -= LEARNING_RATE  # Bias
+    if finished:
+        print("Trained successful")
+    else:
+        print("Did not converged")
+
+
+def evaluate(input_: List) -> int:
+    input_.append(1)
+    if np.dot(input_, _weights) > 0:
+        return 1
+    else:
+        return 0
+
+
 def init_weights():
-    for i in range(0, DIMENSION):
-        weights[i] = randint(-5, 5)
+    for _ in range(0, DIMENSION+1):
+        _weights.append(randint(-5, 5))
 
 
 def parse_training_data(file_name: str) -> List:
@@ -30,7 +63,10 @@ def parse_training_data(file_name: str) -> List:
         for line in file.readlines():
             data = line.split("\t")
             if len(data) is (DIMENSION + 1):
-                training_points.append(data)
+                point = []
+                for i in range(0, len(data)):
+                    point.append(int(data[i]))
+                training_points.append(point)
     return training_points
 
 
