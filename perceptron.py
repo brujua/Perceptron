8f3ceae4821c3e1d472015ac703d9369cf71a5f3
brujua <brunocrisafulli@yahoo.com.ar>
2019-05-13 17:40:35 -0300

    plot decision boundary

--- perceptron.py
+++ perceptron.py
@@ -2,10 +2,13 @@ import sys
 import numpy as np
 from random import randint
 from typing import List
+import matplotlib.pyplot as plt
 
 DIMENSION = 2
 ERROR_ARGS = "Invalid Argument. \nUsage: python " + sys.argv[0] + " <data-set-file> \
 (tab separated, each data point in one line)"
+ERROR_NOT_CONVERGED = "Error, training did not converged, the training data must be non linearly separable"
+PLOT_TITLE = "Perceptron"
 MAX_EPOCHS = 100000
 LEARNING_RATE = 0.1
 
@@ -16,10 +19,14 @@ def main(*args):
     file_name = args[0]
     init_weights()
     training_points = parse_training_data(file_name)
-    train(training_points)
+    trained = train(training_points)
+    if trained:
+        draw_plot(training_points, _weights)
+    else:
+        print(ERROR_NOT_CONVERGED)
 
 
-def train(training_points: List):
+def train(training_points: List) -> bool:
     epoch = 0
     finished = False
     while not finished and epoch < MAX_EPOCHS:
@@ -38,10 +45,7 @@ def train(training_points: List):
                     else:  # result is > than desired
                         _weights[i] -= point[i] * LEARNING_RATE
                         _weights[DIMENSION] -= LEARNING_RATE  # Bias
-    if finished:
-        print("Trained successful")
-    else:
-        print("Did not converged")
+    return finished
 
 
 def evaluate(input_: List) -> int:
@@ -68,6 +72,27 @@ def parse_training_data(file_name: str) -> List:
                     point.append(int(data[i]))
                 training_points.append(point)
     return training_points
+
+
+def draw_plot(data_points: List, weights: List):
+    plt.figure(figsize=(10, 6))
+    plt.grid(True)
+    for point in data_points:
+        plt.plot(point[0], point[1], 'ro' if (point[2] == 1.0) else 'bo')
+
+    slope = -(weights[2] / weights[1]) / (weights[2] / weights[0])
+    intercept = -weights[2] / weights[1]
+    _draw_line(slope, intercept)
+    plt.title(PLOT_TITLE)
+    plt.show()
+
+
+def _draw_line(slope, intercept):
+    """Plot a line from slope and intercept"""
+    axes = plt.gca()
+    x_vals = np.array(axes.get_xlim())
+    y_vals = intercept + slope * x_vals
+    plt.plot(x_vals, y_vals, '--')
 
 
 if __name__ == '__main__':
